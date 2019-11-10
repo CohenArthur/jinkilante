dfe5782eb032902f789f6ec644d4994c485da7dc
cohenarthur <arthur.cohen@epita.fr>
2019-11-10 13:11:54 +0100

    [fix] Fixing use of ProcessUsage;

--- src/ps_parser.rs
+++ src/ps_parser.rs
@@ -1,7 +1,6 @@
-use crate::process_usage;
+use crate::process_usage::ProcessUsage;
 
-pub fn parse_ps_output(mut process: process_usage::ProcessUsage,
-                       ps_output: &str) {
+pub fn parse_ps_output(mut process: ProcessUsage, ps_output: &str) {
 
     let split = ps_output.split_whitespace();
     let words = split.collect::<Vec<&str>>();
@@ -15,7 +14,7 @@ mod tests {
     use super::*;
 
     fn test_parse_zeros() {
-        let mut test_process = process_usage::ProcessUsage::new(12);
+        let mut test_process = ProcessUsage::new(12);
 
         parse_ps_output(test_process, "%CPU %MEM\n 0.0  0.0");
 

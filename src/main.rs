dc7cf86a7b8b30567c43cf57ec97422ecf3a5954
cohenarthur <arthur.cohen@epita.fr>
2019-11-10 12:07:44 +0100

    [add] Adding option parsing to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
-mod options_parsing;
 mod process_usage;
 
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Jinkilante", about, author)]
+struct Arg {
+    #[structopt(short, long,
+                help = "Specific process to keep track of")]
+    process: u32,
+
+    #[structopt(short, long,
+                help = "Number of strikes before a process is terminated",
+                default_value = "10")]
+    strikes: u32,
+
+    #[structopt(short, long,
+                help = "Number of seconds before a process is terminated",
+                default_value = "100")]
+    timeout: u32,
+
+    #[structopt(short, long,
+                help = "Percentage of CPU usage required for a
+                        process to get striked",
+                default_value = "90")]
+    cpu: u8,
+
+    #[structopt(short, long,
+                help = "Percentage of RAM usage required for a
+                        process to get striked",
+                default_value = "90")]
+    mem: u8,
+}
+
 fn main() {
+    let options = Arg::from_args();
+
     println!("Hello, world!");
 }

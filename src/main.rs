16bdd254ffbb246cf62d7fd3bb9dfb323a671e74
cohenarthur <arthur.cohen@epita.fr>
2019-11-10 12:17:06 +0100

    [add] Adding exec_wrapper and fixing arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,38 @@
 mod process_usage;
+mod exec_watcher;
 
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Jinkilante", about, author)]
 struct Arg {
-    #[structopt(short, long,
-                help = "Specific process to keep track of")]
+    #[structopt(short,
+                long,
+                help = "Specific process to keep track of",
+                default_value = "0")]
     process: u32,
 
-    #[structopt(short, long,
+    #[structopt(short,
+                long,
                 help = "Number of strikes before a process is terminated",
                 default_value = "10")]
     strikes: u32,
 
-    #[structopt(short, long,
+    #[structopt(short,
+                long,
                 help = "Number of seconds before a process is terminated",
                 default_value = "100")]
     timeout: u32,
 
-    #[structopt(short, long,
+    #[structopt(short,
+                long,
                 help = "Percentage of CPU usage required for a
                         process to get striked",
                 default_value = "90")]
     cpu: u8,
 
-    #[structopt(short, long,
+    #[structopt(short,
+                long,
                 help = "Percentage of RAM usage required for a
                         process to get striked",
                 default_value = "90")]
@@ -35,5 +42,5 @@ struct Arg {
 fn main() {
     let options = Arg::from_args();
 
-    println!("Hello, world!");
+    exec_watcher::exec_watcher();
 }

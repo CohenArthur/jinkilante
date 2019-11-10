15cd01e3b1b8012d45c88727dddd59c9503862d9
cohenarthur <arthur.cohen@epita.fr>
2019-11-10 13:11:33 +0100

    [add] Adding test and fixing child process name

--- src/exec_watcher.rs
+++ src/exec_watcher.rs
@@ -3,22 +3,39 @@ use std::io::prelude::*;
 use std::process::{Command, Stdio};
 
 use crate::process_usage::ProcessUsage;
+use crate::ps_parser::parse_ps_output;
 
 pub fn exec_watcher(mut process: ProcessUsage) {
     let pid = process.pid.to_string();
-    let process = match Command::new("ps")
+    let child = match Command::new("ps")
                                 .args(&["-p", &pid, "-o", "%cpu,%mem"])
                                 .stdin(Stdio::piped())
                                 .stdout(Stdio::piped())
                                 .spawn() {
         Err(why) => panic!("couldn't spawn ps: {}", why.description()),
-        Ok(process) => process,
+        Ok(child) => child,
     };
 
     let mut output = String::new();
-    match process.stdout.unwrap().read_to_string(&mut output) {
+    match child.stdout.unwrap().read_to_string(&mut output) {
         Err(why) => panic!("couldn't read ps stdout: {}",
                            why.description()),
         Ok(_) => print!("ps responded with:\n{}", output),
+    }
+
+    parse_ps_output(process, &output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_exec_watcher_null() {
+        let mut process = ProcessUsage::new(1);
+
+        exec_watcher(process);
+
+        assert_eq!(process.cpu_usage, 0);
+        assert_eq!(process.mem_usage, 0);
     }
 }

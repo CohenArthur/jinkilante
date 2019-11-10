dbffde85c3cbb6ed5b752beb9a7751b85f8bb893
cohenarthur <arthur.cohen@epita.fr>
2019-11-10 12:38:01 +0100

    [fix] Fixing exec_watcher for a useful behavior

--- src/exec_watcher.rs
+++ src/exec_watcher.rs
@@ -2,40 +2,26 @@ use std::error::Error;
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
 
-static PANGRAM: &'static str =
-"the quick brown fox jumped over the lazy dog\n";
+use crate::process_usage::ProcessUsage;
 
-pub fn exec_watcher() {
-    // Spawn the `wc` command
-    let process = match Command::new("randomshit")
+static PANGRAM: &'static str =
+"hello\n";
+
+pub fn exec_watcher(mut process: ProcessUsage) {
+    let pid = process.get_pid().to_string();
+    let process = match Command::new("ps")
+                                .args(&["-p", &pid, "-o", "%cpu,%mem"])
                                 .stdin(Stdio::piped())
                                 .stdout(Stdio::piped())
                                 .spawn() {
-        Err(why) => panic!("couldn't spawn randomshit: {}", why.description()),
+        Err(why) => panic!("couldn't spawn ps: {}", why.description()),
         Ok(process) => process,
     };
 
-    // Write a string to the `stdin` of `wc`.
-    //
-    // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-    // must have one, we can directly `unwrap` it.
-    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
-        Err(why) => panic!("couldn't write to wc stdin: {}",
+    let mut output = String::new();
+    match process.stdout.unwrap().read_to_string(&mut output) {
+        Err(why) => panic!("couldn't read ps stdout: {}",
                            why.description()),
-        Ok(_) => println!("sent pangram to wc"),
-    }
-
-    // Because `stdin` does not live after the above calls, it is `drop`ed,
-    // and the pipe is closed.
-    //
-    // This is very important, otherwise `wc` wouldn't start processing the
-    // input we just sent.
-
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    let mut s = String::new();
-    match process.stdout.unwrap().read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read wc stdout: {}",
-                           why.description()),
-        Ok(_) => print!("wc responded with:\n{}", s),
+        Ok(_) => print!("ps responded with:\n{}", output),
     }
 }
